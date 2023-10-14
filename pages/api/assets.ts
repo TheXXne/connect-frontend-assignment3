aec8c84b0b1b964bd78d408b4d6889f7b3bcb1f2
TheXXne <jun87250@gmail.com>
2023-10-14 13:31:21 +0900

    assets API 단건 조회 기능 추가

--- pages/api/assets.ts
+++ pages/api/assets.ts
@@ -2,7 +2,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-const ADMIN_PRIVATE_KEY = process.env.ADMINACCOUNT_PRIVATE_KEY;
 const NETWORK: string = "ethereum";
 const AZUKI_SMARTCONTRACT_ADDRESS: string =
   "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
@@ -10,11 +9,17 @@ const CLO_CERTI_SMARTCONTRACT_ADDRESS: string =
   "0x0669E8C36635fce3f26cE510713ccA769ecF8B88";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const sdk = new ThirdwebSDK(NETWORK);
+  const contract = await sdk.getContract(AZUKI_SMARTCONTRACT_ADDRESS);
   if (req.method === "GET") {
-    const sdk = new ThirdwebSDK(NETWORK);
-    const contract = await sdk.getContract(AZUKI_SMARTCONTRACT_ADDRESS);
-    const assets = await contract.erc721.getAll();
-    res.status(200).json({ assets });
+    if (req.query.tokenId) {
+      const tokenId: number = Number(req.query.tokenId);
+      const asset = await contract.erc721.get(tokenId);
+      res.status(200).json({ asset });
+    } else {
+      const assets = await contract.erc721.getAll();
+      res.status(200).json({ assets });
+    }
   } else if (req.method === "POST") {
     res.status(200).json({ message: "POST" });
   } else {
