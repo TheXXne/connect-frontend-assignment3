12a55112caab57a2a02f3d88ba037239d2514f39
TheXXne <jun87250@gmail.com>
2023-10-20 16:03:02 +0900

    assets페이지 api활용하여 정보 노출, 클릭시 페이지 이동

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "bafybeies3odi24wyk3e22rnautr57tiuk3b56nxrd53fxgtvr37abmz5j4.ipfs.cf-ipfs.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
