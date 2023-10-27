326798bac7b3ac984bf095625beef7da8e477faf
TheXXne <jun87250@gmail.com>
2023-10-27 10:56:53 +0900

    Listing 페이지 Header UI

--- src/components/assets/Header.tsx
+++ src/components/assets/Header.tsx
@@ -1,11 +1,32 @@
 import SearchBox from './SearchBox';
 import GridBox from './GridBox';
+import styled from '@emotion/styled';
 
 export default function Header() {
   return (
-    <div>
-      <SearchBox />
-      <GridBox />
-    </div>
+    <HeaderWrap>
+      <InnerDiv>
+        <SearchBox />
+        <GridBox />
+      </InnerDiv>
+    </HeaderWrap>
   );
 }
+
+const HeaderWrap = styled.div`
+  margin: 24px 24px 16px 24px;
+  vertical-align: inherit;
+  max-width: 100%;
+  min-height: 0;
+  min-width: 0;
+  flex-shrink: 0;
+  flex-direction: column;
+  flex-basis: auto;
+  display: flex;
+  -webkit-box-align: stretch;
+  align-items: stretch;
+`;
+
+const InnerDiv = styled.div`
+  display: flex;
+`;
