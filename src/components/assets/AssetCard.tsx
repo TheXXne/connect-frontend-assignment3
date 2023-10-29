88f1582ee8883a8e8f8de40754af70dd1fcb7868
TheXXne <jun87250@gmail.com>
2023-10-29 16:07:42 +0900

    Listing 페이지 asset card hover 효과 적용

--- src/components/assets/AssetCard.tsx
+++ src/components/assets/AssetCard.tsx
@@ -9,7 +9,17 @@ export default function AssetCard(props: any) {
       <Link href={`/assets/${props.asset.metadata.id}`}>
         <AssetCardDiv>
           <AssetImg>
-            <Image src={props.asset.metadata.image} alt="Asset" width="163.75" height="163.75" />
+            <ImageWrap>
+              <Image src={props.asset.metadata.image} alt="Asset" width="163.75" height="163.75" />
+            </ImageWrap>
+            <HoverWrap>
+              <HoverOuter>
+                <HoverInner>
+                  <ButNowIcon>Buy Now</ButNowIcon>
+                  <AddIcon>+</AddIcon>
+                </HoverInner>
+              </HoverOuter>
+            </HoverWrap>
           </AssetImg>
           <AssetInfo>
             <ContextIconWarp>
@@ -37,6 +47,13 @@ const AssetCardWarp = styled.div`
   background: transparent;
   border: 1px solid rgba(22, 22, 26, 0.08);
   border-radius: 16px;
+  -webkit-transition: 0.1s ease-in-out;
+  transition: 0.1s ease-in-out;
+  &:hover {
+    margin-top: -3px;
+    -webkit-transition: 0.1s ease-in-out;
+    transition: 0.1s ease-in-out;
+  }
 `;
 
 const AssetCardDiv = styled.div`
@@ -58,6 +75,93 @@ const AssetImg = styled.div`
   opacity: 1;
   border-radius: 14px;
   border: 1px solid white;
+`;
+
+const ImageWrap = styled.div`
+  width: 163.75px;
+  height: 163.75px;
+`;
+
+const HoverWrap = styled.div`
+  opacity: 0;
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  transition: all 0.08s ease-in-out 0s;
+  &:hover {
+    opacity: 1;
+    background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(22, 22, 26, 0.3) 95%);
+    position: absolute;
+    inset: 0;
+    z-index: 1;
+    transition: all 0.08s ease-in-out 0s;
+  }
+`;
+
+const HoverOuter = styled.div`
+  position: absolute;
+  bottom: 16px;
+  left: 50%;
+  transform: translate(-50%, 10px);
+  opacity: 1;
+  transition: all 0.08s ease-in-out 0s;
+`;
+
+const HoverInner = styled.div`
+  display: flex;
+  flex-flow: row;
+`;
+
+const ButNowIcon = styled.button`
+  margin-right: 8px;
+  margin-bottom: 0;
+  color: rgb(255, 255, 255);
+  background-color: rgb(22, 22, 26);
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  flex-flow: row;
+  position: relative;
+  white-space: nowrap;
+  line-height: 40px;
+  height: 40px;
+  border: 1px solid transparent;
+  min-width: auto;
+  font-size: 14px;
+  border-radius: 12px;
+  padding: 6px 13.5px;
+  font-weight: 700;
+  font-family: inherit;
+  transition: all 0.15s ease-in-out 0s;
+  transform-origin: center center;
+  user-select: none;
+`;
+
+const AddIcon = styled.button`
+  color: rgb(255, 255, 255);
+  background-color: rgb(22, 22, 26);
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  flex-flow: row;
+  position: relative;
+  white-space: nowrap;
+  line-height: 40px;
+  height: 40px;
+  border: 1px solid transparent;
+  min-width: auto;
+  font-size: 14px;
+  border-radius: 12px;
+  padding: 6px 13.5px;
+  font-weight: 700;
+  font-family: inherit;
+  transition: all 0.15s ease-in-out 0s;
+  transform-origin: center center;
+  user-select: none;
 `;
 
 const AssetInfo = styled.div`
@@ -87,9 +191,13 @@ const ContextIconWarp = styled.button`
   width: 32px;
   height: 32px;
   background: transparent;
-  border: 1px solid white;
+  border: 0 solid white;
   border-radius: 12px;
   color: rgba(22, 22, 26, 0.6);
+  &:hover {
+    cursor: pointer;
+    background: rgba(22, 22, 26, 0.08);
+  }
 `;
 
 const ContextIconDiv = styled.div`
