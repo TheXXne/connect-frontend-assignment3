ea6f3a7a57ad711b691a2f377144d28cdae424fa
TheXXne <jun87250@gmail.com>
2023-11-01 09:07:11 +0900

    Redux Saga 적용 시도3 - 기본 셋팅 Provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
-import "../styles/globals.css";
-import { theme } from "@closet-design-system/theme-connect";
-import { ThemeProvider } from "@emotion/react";
+import '../styles/globals.css';
+import { theme } from '@closet-design-system/theme-connect';
+import { ThemeProvider } from '@emotion/react';
+import { Provider } from 'react-redux';
+import store from '../src/store/store';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </Provider>
   );
 }
 
